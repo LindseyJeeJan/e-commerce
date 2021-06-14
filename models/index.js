814be483024df984e1596c0beb27871916fa2173
Lindsey JJ <ljjlauria@mac.com>
2021-06-14 16:06:56 -0700

    updated foreign key reference in join models

--- models/index.js
+++ models/index.js
@@ -12,25 +12,22 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { 
   through: {
     model: ProductTag,
+    foreignKey: 'product_id'
   }, 
-   // Define an alias for when data is retrieved
-  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
   through: {
     model: ProductTag,
+    foreignKey: 'tag_id'
   }, 
-   // Define an alias for when data is retrieved
-  as: 'tags_products'
 });
 
 module.exports = {Product, Category, Tag, ProductTag};
